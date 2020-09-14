a5e3445eb4d998c5700970e9ea8de05a48f6ebd5
Suerta <suerta__@hotmail.com>
2020-09-14 15:50:52 +0800

    [Zhang Jindong] feat: should throw when input is null

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,8 @@
+public class Mommifier {
+    public String mommify(String string) {
+        if (string == null) {
+            throw new IllegalArgumentException("Input should not be null");
+        }
+        return null;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+
+    @Test
+    void should_throw_when_given_null() {
+        Mommifier mommifier = new Mommifier();
+        final Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            String actual = mommifier.mommify(null);
+        });
+        assertEquals(exception.getMessage(), "Input should not be null");
+    }
 }
